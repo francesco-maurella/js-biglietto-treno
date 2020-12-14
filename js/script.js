24d18da8c4699bd9b681c5b83383d3fc083db806
Francesco Maurella <francesco.maurella@live.it>
2020-12-14 18:58:53 +0100

    Aggiornamento stili. FINE

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-var km = parseInt(prompt('Quanti Km percorrerà il passeggero, durante il viaggio?'));
+alert('ticket.train ti da il Benvenuto!\nCompila i campi seguenti per conoscere la tariffa del tuo viaggio.')
+var km = parseInt(prompt('Quanti Km percorrerà il passeggero?'));
 var age = parseInt(prompt('Quanti anni ha il passeggero?'));
 
 if (isNaN(km) || isNaN(age)) {
@@ -8,17 +9,17 @@ if (isNaN(km) || isNaN(age)) {
 
   alert('Grazie! Stiamo elaborando la richiesta.');
 
-  var cost = (km * 0.21).toFixed(2);
+  var cost = (km * 0.21);
 
   if (age <= 18) {
-    cost = Math.floor(cost - (cost * 20) / 100);
+    cost = cost - ((cost * 20) / 100);
   };
 
   if(age >= 65) {
-    cost = Math.floor(cost - (cost * 40) / 100);
+    cost = cost - ((cost * 40) / 100);
   };
 
   var result = document.getElementById('result');
 
-  result.innerHTML = 'Il prezzo del biglietto è di Euro <strong>' + (cost) + '</strong>. Le auguriamo Buon viaggio!';
+  result.innerHTML = 'Il prezzo del biglietto è di <strong>Euro ' + (cost).toFixed(2) + '</strong>. Le auguriamo Buon viaggio!';
 };
