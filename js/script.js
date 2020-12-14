003c990c6c04547ef4b1cf6936258634d27161ae
Francesco Maurella <francesco.maurella@live.it>
2020-12-14 17:28:13 +0100

    Perfezionamento Script / Stili . FINE

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 var km = parseInt(prompt('Quanti Km percorrerà il passeggero, durante il viaggio?'));
 var age = parseInt(prompt('Quanti anni ha il passeggero?'));
 
-alert('Grazie! Stiamo elaborando la richiesta.');
+if (isNaN(km) || isNaN(age)) {
+  alert('Ti preghiamo di utilizzare soltanto caratteri numerici.')
+} else {
 
-var cost = km * 0.21;
+  alert('Grazie! Stiamo elaborando la richiesta.');
 
-if (age <= 18) {
-  cost = Math.floor(cost - (cost * 20) / 100);
-};
+  var cost = Math.ceil(km * 0.21);
 
+  if (age <= 18) {
+    cost = Math.floor(cost - (cost * 20) / 100);
+  };
 
-if(age >= 65) {
-  cost = Math.floor(cost - (cost * 40) / 100);
-};
+  if(age >= 65) {
+    cost = Math.floor(cost - (cost * 40) / 100);
+  };
 
+  var result = document.getElementById('result')
 
-alert('Il prezzo del biglietto è di Euro ' + (cost) + '. Le auguriamo Buon viaggio!');
+  result.innerHTML = 'Il prezzo del biglietto è di Euro <strong>' + (cost) + ',00</strong>. Le auguriamo Buon viaggio!';
+
+}
